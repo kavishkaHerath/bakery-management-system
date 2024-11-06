7c4fb269569c7834ed52f67ab00a63bd430632c5
kavishkaHerath <hmravindu@gmail.com>
2024-11-06 22:43:10 +0530

    Supplier update method add

--- src/main/java/com/erp/bakery/service/SupplierService.java
+++ src/main/java/com/erp/bakery/service/SupplierService.java
@@ -21,4 +21,34 @@ public class SupplierService {
         }
         return supplierRepository.save(supplier);
     }
+
+    public Supplier updateSupplier(Supplier updatedSupplier) {
+        var supplierID = updatedSupplier.getSupplierCode();
+        var phone = updatedSupplier.getPhone();
+        var email = updatedSupplier.getEmail();
+
+        Supplier existingSupplier = supplierRepository.findById(supplierID).
+                orElseThrow(() -> new RuntimeException("Supplier not found"));
+
+        // Check if email is in use by another supplier
+        if (email != null && !email.equals(existingSupplier.getEmail()) &&
+                supplierRepository.existsByEmail(email)) {
+            throw new DuplicateFieldException("Email " + email + " is already in use.");
+        }
+        // Check if phone number is in use by another employee
+        if (phone != null && !phone.equals(existingSupplier.getPhone()) &&
+                supplierRepository.existsByPhone(phone)) {
+            throw new DuplicateFieldException("Phone number " + phone + " is already in use.");
+        }
+
+        // Update allowed fields
+        existingSupplier.setSupplierName(updatedSupplier.getSupplierName());
+        existingSupplier.setCompaneyName(updatedSupplier.getCompaneyName());
+        existingSupplier.setEmail(email);
+        existingSupplier.setPhone(phone);
+        existingSupplier.setAddress(updatedSupplier.getAddress());
+        existingSupplier.setImageUrl(updatedSupplier.getImageUrl());
+
+        return supplierRepository.save(existingSupplier);
+    }
 }
