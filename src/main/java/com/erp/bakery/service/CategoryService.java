1ebe1a040c05dc0e79e5cb19166b180e2f8a90ba
kavishkaHerath <hmravindu@gmail.com>
2024-11-06 23:58:17 +0530

    add Category insert method

--- src/main/java/com/erp/bakery/service/CategoryService.java
+++ src/main/java/com/erp/bakery/service/CategoryService.java
@@ -1,5 +1,7 @@
 package com.erp.bakery.service;
 
+import com.erp.bakery.exception.DuplicateFieldException;
+import com.erp.bakery.model.Category;
 import com.erp.bakery.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,12 @@ import org.springframework.stereotype.Service;
 public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
+
+    public Category addCategory(Category category) {
+        // Check for duplicate name
+        if (categoryRepository.existsByName(category.getCategoryName())) {
+            throw new DuplicateFieldException(category.getCategoryName() + " is already in use.");
+        }
+        return categoryRepository.save(category);
+    }
 }
