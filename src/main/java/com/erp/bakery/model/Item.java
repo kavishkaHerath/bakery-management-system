13ed44254eeaf73249bfbb1de8af59a321c5c516
kavishkaHerath <hmravindu@gmail.com>
2024-11-07 10:42:06 +0530

    Item class add

---
+++ src/main/java/com/erp/bakery/model/Item.java
@@ -0,0 +1,29 @@
+package com.erp.bakery.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "itemTbl", uniqueConstraints = {@UniqueConstraint(columnNames = "itemName")})
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-increment for primary key
+    private Long itemId;
+    @Column(nullable = false, unique = true)
+    private String itemName;
+    @Column(nullable = false)
+    private String displayName;
+    @ManyToOne
+    @JoinColumn(name = "categoryType", referencedColumnName = "categoryId", nullable = false)
+    private Category category;
+    @ManyToOne
+    @JoinColumn(name = "supplierCode", referencedColumnName = "supplierCode", nullable = false)
+    private Supplier supplier;
+    @Column(nullable = false, updatable = false)
+    private LocalDate addDate;
+    @Column(nullable = false)
+    private String addBy;
+}
