07debe2249fa2ed195e40de2fb0c9a3f96cd7b02
kavishkaHerath <hmravindu@gmail.com>
2024-11-07 10:43:24 +0530

    add ItemRepository

---
+++ src/main/java/com/erp/bakery/repository/ItemRepository.java
@@ -0,0 +1,7 @@
+package com.erp.bakery.repository;
+
+import com.erp.bakery.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
