81a7e6742d83edde7f1dbe317b918f505bf56bd2
kavishkaHerath <hmravindu@gmail.com>
2024-12-09 23:14:26 +0530

    change order service class

--- src/main/java/com/erp/bakery/service/OrderService.java
+++ src/main/java/com/erp/bakery/service/OrderService.java
@@ -3,6 +3,7 @@ package com.erp.bakery.service;
 import com.erp.bakery.exception.AccessToModifyException;
 import com.erp.bakery.model.OrderDetail;
 import com.erp.bakery.model.Order;
+import com.erp.bakery.repository.OrderDetailRepository;
 import com.erp.bakery.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,58 +17,60 @@ import java.util.Locale;
 public class OrderService {
     @Autowired
     private final OrderRepository orderRepository;
-//    @Autowired
-//    private final OrderDetailRepository orderDetailRepository;
+
+    @Autowired
+    // private final OrderDetailRepository orderDetailRepository;
 
     public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
 //        this.orderDetailRepository = orderDetailRepository;
     }
 
-//    public Order saveItemOrder(Order order) {
-//        // Generate orderCode
-//        String orderCode = generateItemOrderCode(order.getRequestBy());
-//        order.setOrderCode(orderCode);
-//
-//        order.setRequestDate(LocalDate.now());
-//        if (order.getRequestBy().substring(0, 3).equals("ADM")) {
-//            order.setStatus("A");
-//        } else {
-//            order.setStatus("P");// If the status is 'P', the person who made this order can modify the requirements.
-//        }
-//
-//        var numberOfItems = 0;
-//        var totalPriceOfItem = 0.00;
-//
-//        for (OrderDetail item : order.getOrderDetails()) {
-//            numberOfItems++;
-//            System.out.println(item.getId());
-//            var total = item.getUnitPrice() * item.getQuantity();
-//            totalPriceOfItem += total;
-//            item.setTotalPrice(total);
-//        }
-//        order.setNumberOfItems(numberOfItems);
-//        order.setTotalPrice(totalPriceOfItem);
-//        // Save order and associated orderDetails
-//        orderRepository.save(order);
-//
-//        return order;
-//    }
+    public Order saveItemOrder(Order order) {
+        // Generate orderCode
+        var requestUser = order.getRequestBy().getUserId();
+        String orderCode = generateItemOrderCode(requestUser);
+        order.setOrderCode(orderCode);
 
-//    private String generateItemOrderCode(String requestBy) {
-//        // Get the current date and time
-//        LocalDateTime now = LocalDateTime.now();
-//        // Format the date as YYMMMDD (e.g., 24AUG08)
-//        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyMMMdd", Locale.ENGLISH);
-//        String datePart = now.format(dateFormatter).toUpperCase();
-//
-//        // Format the time as HHMM (e.g., 1040 for 10:40 AM)
-//        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HHmmss");
-//        String timePart = now.format(timeFormatter);
-//
-//        // Generate custom Order Code
-//        return datePart + requestBy + timePart;
-//    }
+        order.setRequestDate(LocalDate.now());
+        if (requestUser.substring(0, 3).equals("ADM")) {
+            order.setStatus("A");
+        } else {
+            order.setStatus("P");// If the status is 'P', the person who made this order can modify the requirements.
+        }
+
+        var numberOfItems = 0;
+        var totalPriceOfItem = 0.00;
+
+        for (OrderDetail item : order.getOrderDetails()) {
+            numberOfItems++;
+            System.out.println(item.getId());
+            var total = item.getUnitPrice() * item.getQuantity();
+            totalPriceOfItem += total;
+            item.setTotalPrice(total);
+        }
+        order.setNumberOfItems(numberOfItems);
+        order.setTotalPrice(totalPriceOfItem);
+        // Save order and associated orderDetails
+        orderRepository.save(order);
+
+        return order;
+    }
+
+    private String generateItemOrderCode(String requestBy) {
+        // Get the current date and time
+        LocalDateTime now = LocalDateTime.now();
+        // Format the date as YYMMMDD (e.g., 24AUG08)
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyMMMdd", Locale.ENGLISH);
+        String datePart = now.format(dateFormatter).toUpperCase();
+
+        // Format the time as HHMM (e.g., 1040 for 10:40 AM)
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HHmmss");
+        String timePart = now.format(timeFormatter);
+
+        // Generate custom Order Code
+        return datePart + requestBy + timePart;
+    }
 //
 //    public Order updateItemsDetails(Order order, String modifyingUser) {
 //        Order existingOrder = orderRepository.findById(order.getOrderCode())
